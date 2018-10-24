1774dccdbb60d22e60124f1ef0faa3fecc6ab7cc
aalinc9 <chingheng113@gmail.com>
2018-10-23 22:45:56 -0400

    just backup

--- carotid/peroformance_anlysis.py
+++ carotid/peroformance_anlysis.py
@@ -1,10 +1,56 @@
 from my_util import data_util
 from carotid import carotid_data_util as cdu
-from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc
+import numpy as np
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, roc_curve, auc
 import matplotlib.pyplot as plt
+import os
 
-target = 'REICA'
-soure = 'ex'
+
+def get_p(cm):
+    TN = cm[0][0]
+    FP = cm[0][1]
+    FN = cm[1][0]
+    TP = cm[1][1]
+    # Sensitivity, hit rate, recall, or true positive rate
+    TPR = TP/(TP+FN)
+    # Specificity or true negative rate
+    TNR = TN/(TN+FP)
+    # Precision or positive predictive value
+    PPV = TP/(TP+FP)
+    # Negative predictive value
+    NPV = TN/(TN+FN)
+    # Fall out or false positive rate
+    FPR = FP/(FP+TN)
+    # False negative rate
+    FNR = FN/(TP+FN)
+    # False discovery rate
+    FDR = FP/(TP+FP)
+    # Overall accuracy
+    ACC = (TP+TN)/(TP+FP+FN+TN)
+    return TPR, TNR, ACC
+
+
+'''
+RCCA
+REICA
+RIICA
+RACA
+RMCA
+RPCA
+REVA
+RIVA
+BA
+LCCA
+LEICA
+LIICA
+LACA
+LMCA
+LPCA
+LEVA
+LIVA
+'''
+target = 'LIVA'
+source = 'exin'
 classifier = 'svm'
 
 # result = cdu.get_result(classifier+'_'+soure+'_'+target+'.csv')
@@ -20,19 +66,32 @@ classifier = 'svm'
 #          label=' (AUC = %0.3f )' % roc_auc,
 #          lw=1, alpha=.8)
 # plt.show()
-
-
+sen =[]
+spe = []
+acc = []
 for inx in range(0, 10, 1):
-    result = cdu.get_result(classifier+'_'+soure+'_'+target+'_'+str(inx)+'.csv')
+    result = cdu.get_result(source+os.sep+classifier+'_'+source+'_'+target+'_'+str(inx)+'.csv')
     label = list(result['label'].values)
     probas_ = result[['0', '1']].values
     predict = list(data_util.labelize(probas_))
-    print(classification_report(label, predict, digits=4))
+    # print(classification_report(label, predict, digits=4))
+    cm = confusion_matrix(label, predict)
+    TPR, TNR, ACC = get_p(cm)
     if inx == 0:
         labels = label
         predicts = predict
     else:
         labels.extend(label)
         predicts.extend(predict)
+    sen.append(TPR)
+    spe.append(TNR)
+    acc.append(ACC)
+
 print('---')
 print(classification_report(labels, predicts, digits=4))
+# print(round(accuracy_score(labels, predicts), 4))
+print(target)
+print(round(np.mean(acc), 4), round(np.std(acc), 4))
+print(round(np.mean(sen), 4), round(np.std(sen), 4))
+print(round(np.mean(spe), 4), round(np.std(spe), 4))
+
